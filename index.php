91afe363255317640d0ab5f7cc77e0288eabbc4b
yourSepuh <awclvn@gmail.com>
2023-09-12 17:14:48 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -7,5 +7,8 @@
 <body>
     <h2>hello world!</h2>
     <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Aperiam nam nostrum magnam? Neque eum libero officia deserunt est commodi hic tempora enim labore illo quis sunt fugit obcaecati, veritatis vero.</p>
+    <li>
+        coba aja
+    </li>
 </body>
 </html>
